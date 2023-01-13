aa436403e3070cdc63fa1c96ef76e05c96c10f3d
satisayknight <ssingh98@uw.edu>
2023-01-13 23:09:02 +0000

    studying for logic

---
+++ sixty_one/sixty_one.py
@@ -0,0 +1,5 @@
+# incremented by -1
+for i in range(100, 0 , -1):
+	print(i, end="bottles of beer on the wall! ") print(i, end="bottles of beer on the wall! ", i, end="bottles of beer on the wall! ")
+print()
+
